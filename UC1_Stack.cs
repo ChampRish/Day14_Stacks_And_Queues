d6ba47ba6ef0c419981b45223447ed7a04fbea1c
RISHABH <gguptarishabh82@gmail.com>
2022-03-16 03:00:13 +0530

    Ability to create a Stack

---
+++ UC1_Stack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_Stacks_And_Queues
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+                Stack<int> st = new Stack<int>();
+
+                st.Push(70);
+                st.Push(30);
+                st.Push(56);
+
+
+                foreach (int ele in st)
+                {
+                    Console.WriteLine(ele);
+                }
+                Console.WriteLine("----------");
+                Console.WriteLine(st.Peek());
+            Console.ReadLine();
+        }
+    }
+}
+
