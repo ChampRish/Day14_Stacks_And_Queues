937391a172e72ad785e53fbe12b872c214ecf44f
RISHABH <gguptarishabh82@gmail.com>
2022-03-16 03:08:07 +0530

    Peak and Pop from the Stack

---
+++ UC2_Peak_Pop.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_Stacks_And_Queues
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> st = new Stack<int>();
+
+            st.Push(70);
+            st.Push(30);
+            st.Push(56);
+
+
+            foreach (int ele in st)
+            {
+                Console.WriteLine(ele);
+            }
+            Console.WriteLine("----------");
+            Console.WriteLine("peek value " + st.Peek());
+            Console.WriteLine("----------");
+            st.Pop();
+            foreach (int ele in st)
+            {
+                Console.WriteLine(ele);
+            }
+            Console.WriteLine("--------");
+
+            Console.WriteLine("peek value " + st.Peek());
+            Console.WriteLine("----------");
+            st.Pop();
+            foreach (int ele in st)
+            {
+                Console.WriteLine(ele);
+            }
+            Console.WriteLine("--------");
+
+            Console.WriteLine("peek value " + st.Peek());
+            Console.WriteLine("----------");
+            st.Pop();
+            foreach (int ele in st)
+            {
+                Console.WriteLine(ele);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
+
